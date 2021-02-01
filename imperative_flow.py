fddbc52e8eb8cef925ee01480ebbac45c5cda1cc
peytonrunyan <prunyan@recyclingpartnership.org>
2021-02-01 12:00:32 -0500

    try imperative flow

---
+++ imperative_flow.py
@@ -0,0 +1,85 @@
+#--------------------------------------------------------------
+# Imports
+#--------------------------------------------------------------
+from prefect import Flow, task, Parameter
+from prefect.run_configs import LocalRun
+from prefect.storage import GitHub
+import prefect
+from prefect.engine.results import LocalResult
+from prefect.tasks.sql_server import SqlServerFetch
+from prefect.tasks.secrets import EnvVarSecret
+#--------------------------------------------------------------
+# Setup
+#--------------------------------------------------------------
+
+result_formatter = LocalResult(
+    dir=prefect.config.STORAGE_PATH,
+    location="{flow_name}/"
+    "{scheduled_start_time:%d-%m_%H-%M-%S}/"
+    "{task_full_name}-{task_run_id}.prefect_result")
+
+#--------------------------------------------------------------
+# Custom task functions
+#--------------------------------------------------------------
+@task(name="dog", result=result_formatter)
+def dog(thing):
+    logger = prefect.context.get("logger")
+    logger.info(thing)
+    return "This is a returned string"
+
+@task(name="view sql")
+def view_sql(sql):
+    logger = prefect.context.get("logger")
+    logger.info(sql)
+#--------------------------------------------------------------
+# Task classes
+#--------------------------------------------------------------
+get_manual_override_rows = """
+--sql
+SELECT * from manual_override
+WHERE yes_no = 'yes'
+and text != 'No text - category selected manually';
+"""
+
+# Get our database items
+sql_task = SqlServerFetch(
+        db_name=prefect.config.sql_server.database,
+        user=prefect.config.sql_server.user,
+        host=prefect.config.sql_server.server,
+        query=get_manual_override_rows,
+        fetch='many',
+        fetch_count=3,
+        result=result_formatter,
+        name="SQL-stuff"
+        # commit: bool = False,
+)
+#--------------------------------------------------------------
+# Flow context
+#--------------------------------------------------------------
+# storage = GitHub(
+#     repo="peyton-trp/prefect-test",
+#     path="simple_flow.py",
+#     secrets=["GITHUB_ACCESS_TOKEN"]
+# )
+
+flow = Flow("imp_flow")
+flow.storage = GitHub(
+    repo="peyton-trp/prefect-test",
+    path="imperative_flow.py",
+    secrets=["GITHUB_ACCESS_TOKEN"]
+)
+# password = EnvVarSecret(prefect.config.sql_server.password_var)
+# thing = Parameter("thing", default=["Thing 1"])
+
+# flow.add_task()
+
+#--------------------------------------------------------------
+# Closing Details
+#--------------------------------------------------------------
+# f.run_config = LocalRun(
+#     env={
+#         "PREFECT__USER_CONFIG_PATH": '/Users/peytonrunyan/TRP/prefect/config.toml'})
+
+
+
+# f.register("cat_flow")
